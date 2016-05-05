bc00338eff26fac9f337b7a1338aba94fed3763c
xiaoguei8888 <bojianqiu@gmail.com>
2016-05-05 23:56:21 +0800

    update

--- app/src/main/java/com/boss/android/xtodo/tasks/TasksActivity.java
+++ app/src/main/java/com/boss/android/xtodo/tasks/TasksActivity.java
@@ -3,6 +3,11 @@ package com.boss.android.xtodo.tasks;
 import android.app.FragmentManager;
 import android.app.LoaderManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 
 import com.boss.android.xtodo.R;
 import com.boss.android.xtodo.base.BaseActivity;
@@ -12,12 +17,26 @@ import com.boss.android.xtodo.data.source.local.TasksLocalDataSource;
 import com.boss.android.xtodo.data.source.remote.TasksRemoteDataSource;
 import com.boss.android.xtodo.util.ActivityUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class TasksActivity extends BaseActivity {
+
+    @Nullable
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @Nullable
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+        ButterKnife.bind(this);
+
+        setupViews();
 
         // Init Main Fragment
         LoaderManager loaderManager = getLoaderManager();
@@ -40,5 +59,27 @@ public class TasksActivity extends BaseActivity {
             return;
         }
         super.onBackPressed();
+    }
+
+    private void setupViews(){
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_list:
+                        break;
+                    case R.id.action_message:
+                        break;
+                    case R.id.action_about:
+                        break;
+                    case R.id.action_settings:
+                        mDrawerLayout.closeDrawers();
+                        break;
+                }
+                Snackbar.make(mDrawerLayout, item.getTitle(), Snackbar.LENGTH_SHORT).show();
+                item.setChecked(true);
+                return true;
+            }
+        });
     }
 }
